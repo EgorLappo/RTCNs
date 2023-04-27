26568e1ee5602883ee6b21f3bf0b5d6d6ddba9c9
Egor Lappo <elappo@stanford.edu>
2023-04-26 17:04:42 -0700

    conversion of rtcns to graphs

--- src/RTCN.hs
+++ src/RTCN.hs
@@ -5,19 +5,19 @@
 
 module RTCN where
 
-import           Data.IntMap.Strict (IntMap, (!))
-import qualified Data.IntMap.Strict as IMap
+import           Data.IntMap.Strict                (IntMap, (!))
+import qualified Data.IntMap.Strict                as IMap
 
--- first lets implement the backwards construction of RTCNs
+import           Data.IntSet                       (IntSet)
+import qualified Data.IntSet                       as ISet
+import           Data.List                         (sort)
 
--- the idea is that after ith events we have l - i lineages that we can re-label from left to right
--- B is a coalescence of two lineges, and R is a reticulation of three lineages
-data BWNode' a = B a a | R a a a
-    deriving (Eq, Show)
+import           Data.Graph.Inductive              (mkGraph)
+import qualified Data.Graph.Inductive.PatriciaTree as G
 
-type BWNode = BWNode' Int
-
-type BWRTCN = [BWNode]
+-- int map from node to lineage, with each lineage recording where it is pointing
+-- the key is that ancestral configurations and antichains are about lineages, not nodes
+type RTCN = IntMap IntSet
 
 data Event = BranchingEvent | ReticulationEvent
     deriving (Eq, Show)
@@ -39,23 +39,52 @@ qProfiles n b
 
 -- now lets generate RTCNs given a profile
 
-rtcnsWithProfile :: Int -> Profile -> [BWRTCN]
-rtcnsWithProfile _ [BranchingEvent] = [[B 1 2]]
-rtcnsWithProfile n (e:es)
-  | e == BranchingEvent = [B i j : r | (i,j) <- pairs n, r <- rtcnsWithProfile (n-1) es]
-  | e == ReticulationEvent = [R i j k : r | (i,j,k) <- triples n, r <- rtcnsWithProfile (n-1) es]
+rtcnsWithProfile :: Int -> Profile -> [RTCN]
+rtcnsWithProfile n' p' = go (n'+1) p' nodes' net'
+  where
+    nodes' = ISet.fromAscList [1..n']
+    net' = IMap.fromList [(i, ISet.empty) | i <- [1..n']]
+    -- insert a new node with two tree lineages for the two remaining labels in ls (always going to me two, min and max)
+    go n [BranchingEvent] nodes net = let
+        (v1,v2) = (ISet.findMin nodes, ISet.findMax nodes)
+      in [IMap.insert n (ISet.fromList [v1, v2]) net]
+    go n (x:xs) nodes net = case x of
+      BranchingEvent    -> do
+        (i,j) <- pairs nodes
+        let newnodes = ISet.insert n $ ISet.delete i $ ISet.delete j nodes
+            newnet   = IMap.insert n (ISet.fromList[i, j]) net
+        go (n+1) xs newnodes newnet
+      ReticulationEvent -> do
+        (i,j,k) <- triples nodes
+        let newnodes = ISet.insert n $ ISet.insert (n+1) $ ISet.delete i $ ISet.delete j $ ISet.delete k nodes
+            newnet   = IMap.insert (n+2) (ISet.fromList [n]) $ IMap.insert (n+1) (ISet.fromList [n]) $ IMap.insert n (ISet.fromList [i,j,k]) net
+        go (n+2) xs newnodes newnet
+    pairs ls = [(i,j) | i <- ISet.toList ls, j <- ISet.toList ls, i < j]
+    triples ls = [(i, j, k) | (i,j) <- pairs ls, k <- ISet.toList ls, k /= i, k /= j]
 
-pairs :: Int -> [(Int, Int)]
-pairs n = [(i,j) | i <- [1..n], j <- [1..n], i < j]
+outDegree :: RTCN -> Int -> Int
+outDegree net i = ISet.size $ net ! i
 
-triples :: Int -> [(Int, Int, Int)]
-triples n = [(i, j, k) | (i,j) <- pairs n, k <- [1..n], k /= i, k /= j]
+tryCollapse :: RTCN -> Int -> RTCN
+tryCollapse net i
+  | outDegree net i /= 1 = net
+  | otherwise = let
+      j = ISet.findMin $ net ! i
+      net' = IMap.delete i net
+      net'' = IMap.map (\es -> if ISet.member i es then ISet.insert j $ ISet.delete i es else es) net'
+    in tryCollapse net'' j
 
--- RTCNs with n leaves and b branchings
-bwrtcns' :: Int -> Int -> [BWRTCN]
-bwrtcns' n b = concat [rtcnsWithProfile n p | p <- qProfiles n b]
+collapse :: RTCN -> RTCN
+collapse net = foldl tryCollapse net (IMap.keys net)
 
--- RTCNs with n leaves and r reticulations
-bwrtcns :: Int -> Int -> [BWRTCN]
-bwrtcns n r = bwrtcns' n (n-r-1)
+rtcns' :: Int -> Int -> [RTCN]
+rtcns' n b = map collapse $ concat [rtcnsWithProfile n p | p <- qProfiles n b]
+
+rtcns :: Int -> Int -> [RTCN]
+rtcns n r = rtcns' n (n-r-1)
+
+rtcnToGraph :: RTCN -> G.Gr Int ()
+rtcnToGraph net = mkGraph nodes edges
+  where nodes = [(i,i) | i <- IMap.keys net]
+        edges = [(i, j, ()) | (i,_) <- nodes, j <- ISet.toList $ net ! i]
 
