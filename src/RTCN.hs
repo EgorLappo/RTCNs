bc42f68750960ff3991acff8d1d5900c31fdb0ae
Egor Lappo <elappo@stanford.edu>
2023-04-27 01:24:44 -0700

    correct code for generating rankable tcns, with collapse functions working

--- src/RTCN.hs
+++ src/RTCN.hs
@@ -1,16 +1,12 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module RTCN where
+module RTCN (RTCN, Lineages, Lineages'(..), rtcns, rtcnToGraph) where
 
 import           Data.IntMap.Strict                (IntMap, (!))
 import qualified Data.IntMap.Strict                as IMap
-
-import           Data.IntSet                       (IntSet)
 import qualified Data.IntSet                       as ISet
-import           Data.List                         (sort)
 
 import           Data.Graph.Inductive              (mkGraph)
 import qualified Data.Graph.Inductive.PatriciaTree as G
@@ -63,56 +59,71 @@ bwrtcns n r = bwrtcns' n (n-r-1)
 -- this would "forget the ranking", but the purpose is to look at what rankings are possible, so it's okay
 -- one could say we are generating all "rankable" TCNs
 
-type RTCN = IntMap IntSet
+data Lineages' a = Leaf | Reticulation a a a | Branching a a | Subdiv a deriving (Eq, Show)
 
-rtcnsWithProfile' :: Int -> Profile -> [RTCN]
-rtcnsWithProfile' n' p' = go (n'+1) p' nodes' net'
+type Lineages = Lineages' Int
+
+type RTCN = IntMap Lineages
+
+rtcnsWithProfile :: Int -> Profile -> [RTCN]
+rtcnsWithProfile n' p' = go (n'+1) p' nodes' net'
   where
     nodes' = ISet.fromAscList [1..n']
-    net' = IMap.fromList [(i, ISet.empty) | i <- [1..n']]
-    -- insert a new node with two tree lineages for the two remaining labels in ls (always going to me two, min and max)
+    net' = IMap.fromList [(i, Leaf) | i <- [1..n']]
     go n [BranchingEvent] nodes net = let
         (v1,v2) = (ISet.findMin nodes, ISet.findMax nodes)
-      in [IMap.insert n (ISet.fromList [v1, v2]) net]
+      in [IMap.insert n (Branching v1 v2) net]
     go n (x:xs) nodes net = case x of
-      BranchingEvent    -> do
+      BranchingEvent -> do
         (i,j) <- pairs nodes
         let newnodes = ISet.insert n $ ISet.delete i $ ISet.delete j nodes
-            newnet   = IMap.insert n (ISet.fromList[i, j]) net
+            newnet   = IMap.insert n (Branching i j) net
         go (n+1) xs newnodes newnet
       ReticulationEvent -> do
         (i,j,k) <- triples nodes
         let newnodes = ISet.insert (n+2) $ ISet.insert (n+1) $ ISet.delete i $ ISet.delete j $ ISet.delete k nodes
-            newnet   = IMap.insert (n+2) (ISet.fromList [n]) $ IMap.insert (n+1) (ISet.fromList [n]) $ IMap.insert n (ISet.fromList [i,j,k]) net
+            newnet   = IMap.insert (n+2) (Subdiv n) $ IMap.insert (n+1) (Subdiv n) $ IMap.insert n (Reticulation i j k) net
         go (n+3) xs newnodes newnet
+
     pairs ls = [(i,j) | i <- ISet.toList ls, j <- ISet.toList ls, i < j]
     triples ls = [(i, j, k) | (i,j) <- pairs ls, k <- ISet.toList ls, k /= i, k /= j]
-
-
-outDegree :: RTCN -> Int -> Int
-outDegree net i = ISet.size $ net ! i
 
 tryCollapse :: RTCN -> Int -> RTCN
 tryCollapse net i
   | not $ IMap.member i net = net
-  | outDegree net i /= 1 = net
-  | otherwise = let
-      j = ISet.findMin $ net ! i
-      net' = IMap.delete i net
-      net'' = IMap.map (\es -> if ISet.member i es then ISet.insert j $ ISet.delete i es else es) net'
-    in tryCollapse net'' j
+  | otherwise = case net ! i of
+      Subdiv j -> let
+          net' = IMap.delete i net
+          net'' = IMap.map (replaceLineageLabel i j) net'
+        in tryCollapse net'' j
+      _ -> net
 
 collapse :: RTCN -> RTCN
 collapse net = foldl tryCollapse net (IMap.keys net)
 
 rtcns' :: Int -> Int -> [RTCN]
-rtcns' n b = concat [rtcnsWithProfile' n p | p <- qProfiles n b]
+rtcns' n b = concat [rtcnsWithProfile n p | p <- qProfiles n b]
 
 rtcns :: Int -> Int -> [RTCN]
 rtcns n r = rtcns' n (n-r-1)
 
+-- helper functions
+
 rtcnToGraph :: RTCN -> G.Gr Int ()
 rtcnToGraph net = mkGraph nodes edges
   where nodes = [(i,i) | i <- IMap.keys net]
-        edges = [(i, j, ()) | (i,_) <- nodes, j <- ISet.toList $ net ! i]
+        edges = [(i, j, ()) | (i,_) <- nodes, j <- getLineages $ net ! i]
 
+getLineages :: Lineages' a -> [a]
+getLineages Leaf                 = []
+getLineages (Reticulation i j k) = [i,j,k]
+getLineages (Branching i j)      = [i,j]
+getLineages (Subdiv i)           = [i]
+
+replaceLineageLabel :: Eq a => a -> a -> Lineages' a -> Lineages' a
+replaceLineageLabel x y l = case l of
+    Leaf               -> Leaf
+    Reticulation i j k -> Reticulation (f i) (f j) (f k)
+    Branching i j      -> Branching (f i) (f j)
+    Subdiv i           -> Subdiv (f i)
+  where f z = if z == x then y else z
