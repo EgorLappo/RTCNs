51abcbe0b7dbfec9f316a98d46c0e23f1180b9a9
Egor Lappo <elappo@stanford.edu>
2023-04-27 01:10:05 -0700

    code to generate rankable tcns as dags

--- src/RTCN.hs
+++ src/RTCN.hs
@@ -15,9 +15,14 @@ import           Data.List                         (sort)
 import           Data.Graph.Inductive              (mkGraph)
 import qualified Data.Graph.Inductive.PatriciaTree as G
 
--- int map from node to lineage, with each lineage recording where it is pointing
--- the key is that ancestral configurations and antichains are about lineages, not nodes
-type RTCN = IntMap IntSet
+-- backward construction (proper)
+
+data BWNode' a = B a a | R a a a
+     deriving (Eq, Show)
+
+type BWNode = BWNode' Int
+
+type BWRTCN = [BWNode]
 
 data Event = BranchingEvent | ReticulationEvent
     deriving (Eq, Show)
@@ -37,10 +42,31 @@ qProfiles n b
          | b' == n' = [[BranchingEvent | _ <- [1..n']]]
          | otherwise = fmap (BranchingEvent :) (qProfiles' (n'-1) (b'-1)) ++ fmap (ReticulationEvent :) (qProfiles' (n'-1) b')
 
--- now lets generate RTCNs given a profile
+bwtcnsWithProfile :: Int -> Profile -> [BWRTCN]
+bwtcnsWithProfile _ [BranchingEvent] = [[B 1 2]]
+bwtcnsWithProfile n (e:es)
+  | e == BranchingEvent = [B i j : r | (i,j) <- pairs n, r <- bwtcnsWithProfile (n-1) es]
+  | e == ReticulationEvent = [R i j k : r | (i,j,k) <- triples n, r <- bwtcnsWithProfile (n-1) es]
+  where
+    pairs n = [(i,j) | i <- [1..n], j <- [1..n], i < j]
+    triples n = [(i, j, k) | (i,j) <- pairs n, k <- [1..n], k /= i, k /= j]
 
-rtcnsWithProfile :: Int -> Profile -> [RTCN]
-rtcnsWithProfile n' p' = go (n'+1) p' nodes' net'
+ -- RTCNs with n leaves and b branchings
+bwrtcns' :: Int -> Int -> [BWRTCN]
+bwrtcns' n b = concat [bwtcnsWithProfile n p | p <- qProfiles n b]
+
+-- RTCNs with n leaves and r reticulations
+bwrtcns :: Int -> Int -> [BWRTCN]
+bwrtcns n r = bwrtcns' n (n-r-1)
+
+-- now let's generate something that looks like a DAG
+-- this would "forget the ranking", but the purpose is to look at what rankings are possible, so it's okay
+-- one could say we are generating all "rankable" TCNs
+
+type RTCN = IntMap IntSet
+
+rtcnsWithProfile' :: Int -> Profile -> [RTCN]
+rtcnsWithProfile' n' p' = go (n'+1) p' nodes' net'
   where
     nodes' = ISet.fromAscList [1..n']
     net' = IMap.fromList [(i, ISet.empty) | i <- [1..n']]
@@ -56,17 +82,19 @@ rtcnsWithProfile n' p' = go (n'+1) p' nodes' net'
         go (n+1) xs newnodes newnet
       ReticulationEvent -> do
         (i,j,k) <- triples nodes
-        let newnodes = ISet.insert n $ ISet.insert (n+1) $ ISet.delete i $ ISet.delete j $ ISet.delete k nodes
+        let newnodes = ISet.insert (n+2) $ ISet.insert (n+1) $ ISet.delete i $ ISet.delete j $ ISet.delete k nodes
             newnet   = IMap.insert (n+2) (ISet.fromList [n]) $ IMap.insert (n+1) (ISet.fromList [n]) $ IMap.insert n (ISet.fromList [i,j,k]) net
-        go (n+2) xs newnodes newnet
+        go (n+3) xs newnodes newnet
     pairs ls = [(i,j) | i <- ISet.toList ls, j <- ISet.toList ls, i < j]
     triples ls = [(i, j, k) | (i,j) <- pairs ls, k <- ISet.toList ls, k /= i, k /= j]
+
 
 outDegree :: RTCN -> Int -> Int
 outDegree net i = ISet.size $ net ! i
 
 tryCollapse :: RTCN -> Int -> RTCN
 tryCollapse net i
+  | not $ IMap.member i net = net
   | outDegree net i /= 1 = net
   | otherwise = let
       j = ISet.findMin $ net ! i
@@ -78,7 +106,7 @@ collapse :: RTCN -> RTCN
 collapse net = foldl tryCollapse net (IMap.keys net)
 
 rtcns' :: Int -> Int -> [RTCN]
-rtcns' n b = map collapse $ concat [rtcnsWithProfile n p | p <- qProfiles n b]
+rtcns' n b = concat [rtcnsWithProfile' n p | p <- qProfiles n b]
 
 rtcns :: Int -> Int -> [RTCN]
 rtcns n r = rtcns' n (n-r-1)
